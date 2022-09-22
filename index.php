dd32b9c8d3d55424e169a9dfaf63a2e4e83447bf
Emily McAllister <apocritas@gmail.com>
2022-09-22 10:09:33 -0400

    added targetting

--- index.php
+++ index.php
@@ -112,7 +112,6 @@
 			}
 		}
 
-		//TODO fix this shameful wet code.
 		function display_attacks(){
 			for($i = 0; $i < 4; $i++){
 				if($this->petArray[$i]->current_hp >0)
@@ -135,27 +134,27 @@
 	}
 
 	class Opponent extends Combatant{
-		//image handling has to change for this
+		//image handling has to change for this for a couple reasons:
+		//if the image is of a pet then switch right to left 
+		//if the image is something else then we need an entirely new url
 	}
 
 	class OpponentParty{
 		//unlike battle party which requires exactly 4 there can be any number of opponents in battle. 
-		//Doing it as an array whose length is not determined until execution. maybe linked lists are better but i haven't done one in ages so
 
-		
-		public $opponent_list;
+		public $opponent_list = array();
 
 		function __construct($opponent_list){
 			$this->opponent_list = $opponent_list;
 		}
 
-		function get_opponent_names(){
-			$opponent_names = array();
+		function get_valid_targets(){
+			$targets = ""; 
 			foreach ($this->opponent_list as $opponent){
 				if ($opponent->current_hp >0)
-					array_push($opponent_names, $opponent->name);
-
+					$targets .= "<option value='" . $opponent->name . "'>" . $opponent->name . "</option> \n";
 			}
+			return $targets;
 		}
 		
 
@@ -169,15 +168,16 @@
 	$pet3 = new Combatant("Luikar", "draik_brown", 1, 3, 3, 0);
 	$pet4 = new Combatant("Cazarus", "draik_mutant", 1, 3, 3, 10);
 
-	$opponent1 = new Opponent("Bad Guy", "acara_blue", 1, 1, 1, 1);
-	$opponent2 = new Opponent("Bad Guy", "acara_blue", 1, 1, 1, 1);
+	$opponent1 = new Opponent("Bad Guy1", "acara_blue", 1, 1, 1, 1);
+	$opponent2 = new Opponent("Bad Guy2", "aisha_blue", 1, 1, 1, 1);
 
 
 	$battleparty = new BattleParty($pet1, $pet2, $pet3, $pet4);
 	$opponentparty = new OpponentParty([$opponent1, $opponent2]);
 
 
-//TODO display stuff will all go in its own nice little class
+//TODO display stuff will all go in its own nice little class, split out from the combatant/party classes. 
+// this is easier for quick testing, i usually do display classes last
 echo ("<table><tr>");
 $battleparty->display_images();
 echo ("</tr><tr>");
@@ -187,10 +187,10 @@ $battleparty->display_hp();
 echo ("</tr><tr>");
 $battleparty->display_attacks();
 echo ("</tr><tr>");
-$opponent_list = $opponentparty->get_opponent_names();
+//TODO this shouldn't display for KO'd party members
+$targets = $opponentparty->get_valid_targets();
 for($i = 0; $i <4; $i++){
-	echo ("
-	");
-
+	echo ("<td><select id='pet" . ($i + 1) . "_target'>");
+	echo $targets . "</td>";
 }
 ?>
